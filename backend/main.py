cb4cfde3f3118e583cbb427ef9c0fb4492b7f8bd
cbdewolf <cbdewolf@gmail.com>
2025-04-10 12:57:01 -0500

    added middleware to main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from application_routes import application_router
 
 app = FastAPI()
 app.include_router(application_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 static_path = os.path.join(os.path.dirname(__file__), "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
