03beb813e3ca30dcb9f3859d43ea31d7157f5c09
cbdewolf <cbdewolf@gmail.com>
2025-04-25 10:46:50 -0700

    added homepage route, edited css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard';
-import { Routes, Route } from 'react-router-dom';
-import Reminders from './pages/Reminders';
-import UserHome from './pages/UserHome';
-import Companies from './pages/Companies';
+import React, { useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import Reminders from "./pages/Reminders";
+import UserHome from "./pages/UserHome";
+import Companies from "./pages/Companies";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/reminders" element={<Reminders />} />
-        <Route path="/userhome" element={<UserHome />} />
-        <Route path="/companies" element={<Companies />} />
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/reminders" element={<Reminders />} />
+      <Route path="/userhome" element={<UserHome />} />
+      <Route path="/companies" element={<Companies />} />
+    </Routes>
+  );
 }
-
 
 export default App;
