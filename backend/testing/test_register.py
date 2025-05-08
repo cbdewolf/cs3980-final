2df6a053370c32beb4130f717b249d2d7a96da8b
cbdewolf <cbdewolf@gmail.com>
2025-05-08 13:53:28 -0500

    fixed tests

--- backend/testing/test_register.py
+++ backend/testing/test_register.py
@@ -3,28 +3,39 @@ from fastapi.testclient import TestClient
 
 from backend.main import app
 
+
 @pytest.fixture(scope="module")
 def client():
     # Using "with" ensures the app.lifespan() async generator runs
     with TestClient(app) as c:
         yield c
 
+
 def test_register_endpoint(client):
+    import random
+
+    tested = [1]
+    test_number = 1
+    while test_number in tested:
+        test_number = random.randint(1, 1000)
+        if test_number not in tested:
+            tested.append(test_number)
+            break
+    print(f"Testing with testuser{test_number}")
+
     test_user = {
         # You may have to change each copy of the username each time you test
         # So if "testuser1" isn't working, try "testuser2" and so on
-        "username": "testuser1",
-        "password": "testpassword123"
+        "username": f"testuser{test_number}",
+        "password": "testpassword123",
     }
     response = client.post("/api/users/register", json=test_user)
     assert response.status_code == 200
-    assert response.json()["username"] == "testuser1"
+    assert response.json()["username"] == f"testuser{test_number}"
+
 
 def test_register_duplicate_username(client):
-    duplicate = {
-        "username": "testuser1",
-        "password": "anotherpassword"
-    }
+    duplicate = {"username": "testuser1", "password": "anotherpassword"}
     # Now that the first user exists, this should 400
     response = client.post("/api/users/register", json=duplicate)
     assert response.status_code == 400
